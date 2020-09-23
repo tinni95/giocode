e232d9b201f79db153489617b6b12b23e5a3d3b3
tinni95 <g.damico241095@gmail.com>
2020-09-23 03:02:16 +0200

    cursoradded cursor

--- src/App.js
+++ src/App.js
@@ -4,11 +4,71 @@ import "./App.css";
 import TopBar from "./components/TopBar/TopBar";
 import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import classNames from "classnames";
 
+const Cursor = () => {
+  const [position, setPosition] = React.useState({ x: 0, y: 0 });
+  const [hidden, setHidden] = React.useState(false);
+  const [clicked, setClicked] = React.useState(false);
+  React.useEffect(() => {
+    addEventListeners();
+    return () => removeEventListeners();
+  }, []);
+
+  const addEventListeners = () => {
+    document.addEventListener("mousemove", onMouseMove);
+    document.addEventListener("mouseenter", onMouseEnter);
+    document.addEventListener("mouseleave", onMouseLeave);
+    document.addEventListener("mousedown", onMouseDown);
+    document.addEventListener("mouseup", onMouseUp);
+  };
+
+  const removeEventListeners = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseenter", onMouseEnter);
+    document.removeEventListener("mouseleave", onMouseLeave);
+    document.removeEventListener("mousedown", onMouseDown);
+    document.removeEventListener("mouseup", onMouseUp);
+  };
+
+  const onMouseLeave = () => {
+    setHidden(true);
+  };
+
+  const onMouseEnter = () => {
+    setHidden(false);
+  };
+  const onMouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const onMouseDown = () => {
+    setClicked(true);
+  };
+
+  const onMouseUp = () => {
+    setClicked(false);
+  };
+
+  const cursorClasses = classNames("cursor", {
+    "cursor--hidden": hidden,
+    "cursor--clicked": clicked,
+  });
+  return (
+    <div
+      className={cursorClasses}
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    />
+  );
+};
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Cursor />
       <Switch>
         <Route component={Home} path={"/"} exact />
       </Switch>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,38 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
+import Giovanni from "../../assets/giovanni.png";
+
 const Home = () => {
   return (
-    <Grid container justify="center" alignItems="center">
-      <p>HOME</p>
+    <Grid
+      container
+      className={"homeContainer"}
+      justify="center"
+      alignItems="center"
+      direction="column"
+    >
+      <div style={{ width: 400 }}>
+        <img src={Giovanni} className={"Giocode"} />
+
+        <p className={"Title"}>GIOVANNI D'AMICO</p>
+
+        <p className={"subTitle"}>FULL STACK DEVELOPER</p>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="center"
+          style={{ paddingLeft: 80, paddingRight: 80, marginBottom: 40 }}
+          direction="row"
+        >
+          <p>24/10/1995</p>
+          <p>Italy</p>
+        </Grid>
+        <i className={"quote"}>
+          “Being a creative person, I easily find effective ways to solve
+          problems entailing products’ infrastructure”
+        </i>
+      </div>
     </Grid>
   );
 };
