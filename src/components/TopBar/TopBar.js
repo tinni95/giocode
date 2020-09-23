c67b05dddc0418c57c3ded8c3fc3d38a61b15fea
tinni95 <g.damico241095@gmail.com>
2020-09-23 02:04:15 +0200

    sky

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,20 +1,37 @@
 import React, { useRef } from "react";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Grid, Typography } from "@material-ui/core";
-
+import Sky from "react-sky";
 import "./styles.css";
+
+import BallBig from "../../assets/ball_big.png";
+import BallSmall from "../../assets/ball_small.png";
 
 const TopBar = () => {
   const wrapperRef = useRef();
+  const skyRef = useRef();
   const menuRef = useRef();
   const menuList = useRef();
+
   const handleClick = () => {
     wrapperRef.current.classList.toggle("is-menu-open");
     menuRef.current.classList.toggle("open");
     menuList.current.classList.toggle("open");
+    skyRef.current.classList.toggle("open");
   };
+
   return (
     <div>
+      <div ref={skyRef} className={"sky"}>
+        <Sky
+          images={{
+            0: BallSmall,
+          }}
+          how={12}
+          time={40}
+          size={40}
+        />
+      </div>
       <div ref={wrapperRef} className={"wrapper"}>
         <Grid
           onClick={() => handleClick()}
@@ -29,6 +46,7 @@ const TopBar = () => {
           </div>
         </Grid>
       </div>
+
       <div className={"centerMenu"} ref={menuList}>
         <p className={"menuItem"}>ABOUT ME</p>
         <p className={"menuItem"}>MY STACK</p>
